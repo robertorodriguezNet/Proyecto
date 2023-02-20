ab6577b1799648d8f5a660db45abf71fe771c8ba
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-02-20 19:04:42 +0100

    Lista abierta: spinner OK

--- app/src/main/java/dam/proyecto/activities/lista/ListaListaFragment.java
+++ app/src/main/java/dam/proyecto/activities/lista/ListaListaFragment.java
@@ -6,17 +6,21 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.os.ConditionVariable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import dam.proyecto.R;
 import dam.proyecto.activities.compras.ComprasActivity;
+import dam.proyecto.database.entity.ComercioEntity;
 import dam.proyecto.database.entity.NombreCompraEntity;
+import dam.proyecto.database.repositories.ComercioRespository;
 import dam.proyecto.database.repositories.NombreCompraRepository;
 import dam.proyecto.utilities.Preferencias;
 
@@ -28,9 +32,26 @@ public class ListaListaFragment extends Fragment {
     // Componentes
     TextView lblNombreDeLaCompra;
     ImageView btnSalir;
+    Spinner spinner;
+
+    // Datos
+    // NombreCompraEntity
+    NombreCompraEntity nombreCompra;                      // Es la compra que se está editando
+    // Repositorios
+    ComercioRespository comercioRespository;
+    NombreCompraRepository nombreCompraRepository;
+    // Colección de comercios para el spinner
+    ArrayList<ComercioEntity> dataComercio;
 
     public ListaListaFragment() {
-        // Required empty public constructor
+
+        // Cargar los datos de los comercios
+        comercioRespository = new ComercioRespository( getContext() );
+        dataComercio = comercioRespository.getAll();
+
+        // repositorio para el nombre de la compra
+        nombreCompraRepository = new NombreCompraRepository( getContext() );
+
     }
 
 
@@ -46,32 +67,74 @@ public class ListaListaFragment extends Fragment {
         // Obtenemos el id de la compra desde las preferencias
         String idCompra = Preferencias.getListaAbiertaId(this.getActivity());
 
-        // Inicializar componente
-        lblNombreDeLaCompra = (TextView) view.findViewById(R.id.fla_tv_nombreCompra);
-        btnSalir = (ImageView) view.findViewById(R.id.fla_img_cerrar);
+
 
         // Solo si la compra no es nula
         if (idCompra != null) {
 
             // Obtener el objeto Nombre de la compra.
             // En NombreCompraEntity se establece el comercio
-            NombreCompraRepository repository = new NombreCompraRepository(context);
-            NombreCompraEntity nombreCompra = repository.getById(idCompra);
+            // Inicilizar el NombreDeLaCompra
+            nombreCompra = nombreCompraRepository.getById( idCompra );
+
+            // Inicializar componente
+            lblNombreDeLaCompra = (TextView) view.findViewById(R.id.fla_tv_nombreCompra);
+            btnSalir = (ImageView) view.findViewById(R.id.fla_img_cerrar);
+            spinner =  (Spinner) view.findViewById(R.id.fla_spn_seleccionarComercio);
+
+            // Spinner
+            ArrayAdapter<ComercioEntity> adapter = new ArrayAdapter<>(
+                    context,
+                    androidx
+                            .appcompat
+                            .R.layout.support_simple_spinner_dropdown_item,
+                    dataComercio
+            );
+            spinner.setAdapter(adapter);
+            spinner.setSelection( getSpinnerId() );
 
             // Nombre de la compra
             lblNombreDeLaCompra.setText(nombreCompra.getNombre().toString());
+
+            // Botón salir
+            btnSalir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(context, ComprasActivity.class));
+                }
+            });
         }
 
-        // Botón salir
-        btnSalir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(context, ComprasActivity.class));
-            }
-        });
+
 
 
         return view;
+    }
+
+    /**
+     * Devuelve el id del spinner que se corresponde con el id del comercio.
+     * Hay que tener en cuenta que el spinner comienza a contar por 1.
+     * @return el id del spinner
+     */
+    private int getSpinnerId(){
+        // Establecer el ítem por defecto
+        // Obtener el nombre del comercio
+        String nComercio = comercioRespository
+                .getNombreComercio(
+                        nombreCompra.getComercio()
+                );
+
+        int idComercio = 1;
+        //Buscamos el comercio en la dataComercio
+        boolean nombre = false;
+        ComercioEntity c = null;
+        int count = 0;
+        while( !nombre && (count < dataComercio.size() ) ){
+            c = dataComercio.get( count );
+            nombre = (c.getName().equals(nComercio));
+            count++;
+        }
+        return( ( c == null ) ? 1 : --count );
     }
 
 }
