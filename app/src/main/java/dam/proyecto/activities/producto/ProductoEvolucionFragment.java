b57a376b28ebef855fee36ddf8c9b7ed20ab917f
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-03-17 17:32:14 +0100

    Inicio del gráfico

--- app/src/main/java/dam/proyecto/activities/producto/ProductoEvolucionFragment.java
+++ app/src/main/java/dam/proyecto/activities/producto/ProductoEvolucionFragment.java
@@ -8,7 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import dam.proyecto.R;
+import dam.proyecto.controllers.CompraController;
+import dam.proyecto.controllers.ProductoController;
+import dam.proyecto.database.entity.CompraEntity;
+import dam.proyecto.database.entity.ProductoEntity;
 
 /**
  * Muestra la evolución del precio del producto en el comercio dado
@@ -34,5 +40,40 @@ public class ProductoEvolucionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_producto_evolucion, container, false);
+    }
+
+    /**
+     * Método que obtiene los datos necesarios para mostrar los gráficos.
+     * Debe mostrar todas las compras del producto que se está editando.
+     */
+    private ArrayList<CompraEntity> getDatos() {
+
+        CompraController compraController = new CompraController(getContext());
+
+        // La compra la obtenemos a partir de los argumentos
+        // que recibe el fragment
+        assert getArguments() != null;
+        CompraEntity compra = compraController
+                .getById(
+                        getArguments()
+                                .getString("id")
+                );
+
+
+        // Obtener el producto de la compra
+        ProductoEntity producto = ProductoController
+                .getById(
+                        compra.getProducto(),
+                        getContext()
+                );
+
+        // Tenemos que obtener todas las compras de un producto.
+        // Sólo necesitamos el precio.
+        // Da igual el comercio.
+        return compraController
+                .getCompraByProducto(
+                        producto.getId()
+                );
+
     }
 }
---
+++ app/src/main/java/dam/proyecto/activities/producto/classes/Grafico.java
@@ -0,0 +1,26 @@
+package dam.proyecto.activities.producto.classes;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Esta clase dibuja el gráfico
+ *
+ * @author Roberto Rodríguez Jiménez
+ * @since 17/03/2023
+ * @version 2023.03.17
+ */
+public class Grafico extends View {
+
+
+    public Grafico(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+}
